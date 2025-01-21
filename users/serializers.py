562a8ae8cfcc4045659c93f9d46fcb95d431394d
holdlemon <d.vajov2010@yandex.ru>
2025-01-21 20:25:28 +0300

    Настроены маршруты приложения Пользователь, а также сериализатор, права и контролеры

---
+++ users/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Хешируем пароль с использованием set_password
+        user = User(
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        # Хешируем пароль при обновлении, если пароль предоставлен
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
+
+
+class UserCommonSerializer(serializers.ModelSerializer):
+    """Сериализатор для пользователя с ограниченным набором полей"""
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        # Хешируем пароль с использованием set_password
+        password = validated_data.pop("password", None)
+        user = User(email=validated_data["email"])
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        # Хешируем пароль при обновлении, если пароль предоставлен
+        password = validated_data.pop("password", None)
+        if password:
+            instance.set_password(password)  # Хешируем пароль
+            instance.save()  # Сохраняем изменения после установки пароля
+        return super().update(instance, validated_data)
